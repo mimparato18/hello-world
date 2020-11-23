a42f8acb9d09567b3007b1f265c0df8e641de02e
marku <marku@Marco-1070>
2020-11-23 17:38:10 +0100

    Added print Added Developer print

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello world!");
+        System.out.print("This should appear on the Developer branch");
     }
     
 }
