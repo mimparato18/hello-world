15ef41f3bd8b3748ecb2a91e7770e3fde9ef060c
Marco I <m.imparato18@studenti.unisa.it>
2020-11-27 12:50:37 +0100

    Updated main branch with a NewClass and a Test class

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author marku
+ */
+public class NewClass {
+    String test;
+    int banane;
+    
+}
---
+++ HelloWorld/src/test/java/com/mycompany/helloworld/NewClassTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworld;
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marku
+ */
+public class NewClassTest {
+    
+    public NewClassTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        
+        // fail("The test case is a prototype.");
+    }
+    
+}
